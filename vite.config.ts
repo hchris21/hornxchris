a853f19481c845c2bd622ed71e603b1acc1a4ee2
hchris21 <hornchris21@yahoo.com>
2023-10-02 09:47:31 +0300

    refactor(): update base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
+
+  if (command !== "serve") {
+    config.base = "hornxchris";
+  }
+
+  return config;
 });
